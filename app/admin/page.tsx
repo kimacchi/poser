9fca5e079cb10621dfe6fc890508456500954240
Ahmet Ergül <shiftusu2@gmail.com>
2023-07-31 16:21:35 +0300

    added an admin page with no functionality yet

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const photos = () => {
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <div className="w-2/5 h-5/6 border-4 rounded-3xl border-white"></div>
+    </div>
+  )
+}
+
+export default photos
