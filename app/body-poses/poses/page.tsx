7ed3daeab9df984e50faf88aaabd232727b06778
Ahmet Ergül <shiftusu2@gmail.com>
2023-08-07 15:17:50 +0300

    sdf

--- app/body-poses/poses/page.tsx
+++ app/body-poses/poses/page.tsx
@@ -3,10 +3,28 @@
 import React, {useState, useEffect} from 'react'
 import { useSearchParams} from "next/navigation"
 import PocketBase from 'pocketbase';
+import Image from 'next/image';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
+interface image{
+    age: string,
+    clothe: string,
+    collectionId: string,
+    collectionName: string,
+    created: Date,
+    gender: string,
+    id: string,
+    picture: string,
+    updated: Date
+}
+
 const Page = () => {
+
+    const [images, setImages] = useState<any[]>();
+
+    // * Below code is responsible for filtering through the images.
+
     const searchParams = useSearchParams();
     const tempAge = searchParams.get("age");
     const tempClothe = searchParams.get("clothe");
@@ -27,17 +45,41 @@ const Page = () => {
 
         const localFunc = async () => {            
             const records = await pb.collection("poses").getFullList({
-                filter: `${clotheFilter} ${ageFilter && "&&"} ${ageFilter} ${genderFilter && "&&"} ${genderFilter}`.trim()
+                filter: `${clotheFilter} ${ageFilter && "&&"} ${ageFilter} ${genderFilter && "&&"} ${genderFilter} && picture != null`.trim()
             })
+            const temp: (Record<string, string> | image)[] = records
+            setImages(records);
             console.log(records)
         }
         localFunc().catch(error=>console.log(error))
     })
+
+    // * Below code is responsible for managing the slideshow.
+
+    const generateURL = (col:string, id:string, name:string)=>{
+        console.log(`http://127.0.0.1:8090/api/files/${col}/${id}/${name}`)
+        return `http://127.0.0.1:8090/api/files/${col}/${id}/${name}`
+    }
+
     return (
         <div>
-            {clothe}
-            {age}
-            {gender}
+            <div className='p-4 h-5/6 w-2/5 border-4 rounded-3xl '>
+                <div className='w-full h-full relative' >
+                    {
+                        images?.map(ele=>{
+                            return (
+                                <Image
+                                    alt={ele.picture}
+                                    src={generateURL(ele.collectionId, ele.id, ele.picture)}
+                                    fill
+                                    objectFit='contain'
+                                    key={ele.id}
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
